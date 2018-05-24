cc754fad1cbcd9eb350cb805291bab3e8b40d172
zaid <zaidhusseini@gmail.com>
2018-05-23 19:17:27 -0700

    Create table structure and fake data for crypto price ranks

---
+++ client/src/components/CryptoTable.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Table = (props)=> {
+
+       return (<table>
+                  <tr>
+                    <th>Rank</th>
+                    <th>Crypto</th>
+                    <th>Symbol</th>
+                    <th>Price</th>
+                    <th>Market Cap</th>
+                    <th>% Change</th>
+                  </tr>
+                  {props.cryptos.map(({rank, name, symbol, price, marketCap, priceChange24hr})=> <Crypto rank={rank} name={name} symbol={symbol} price={price} marketCap={marketCap} priceChange24hr={priceChange24hr} />)}
+                </table>)
+
+}
+
+
+const Crypto = (props)=> {
+
+  return (<tr>
+            <td>{props.rank}</td>
+            <td>{props.name}</td>
+            <td>{props.symbol}</td>
+            <td>{props.price}</td>
+            <td>{props.marketCap}</td>
+            <td>{props.priceChange24hr}</td>
+          </tr>)
+
+}
+
+export default Table;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Table from './components/CryptoTable.jsx'
 
 
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {};
+    this.state = {
+      cryptos: [
+        { rank: 1,
+          name: 'Bitcoin',
+          symbol: 'BTC',
+          price: 7958,
+          marketCap: 350,
+          priceChange24hr: 20
+        },
+        { rank: 2,
+          name: 'Ethereum',
+          symbol: 'ETH',
+          price: 650,
+          marketCap: 80,
+          priceChange24hr: -10
+        },
+        { rank: 3,
+          name: 'Ripple',
+          symbol: 'XRP',
+          price: 0.5,
+          marketCap: 30,
+          priceChange24hr: -10
+        }
+      ]
+    };
   }
 
   render(){
-    return (<div>
-            yo! This is your React App
+    return (<div className="container">
+              <div className="navigation">
+              </div>
+              <div className="coin-table">
+                <Table cryptos={this.state.cryptos} />
+              </div>
             </div>)
   }
 
