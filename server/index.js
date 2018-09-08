5e058e439c3592ad8e383b51209136ff8fd2cff9
zaid <zaidhusseini@gmail.com>
2018-09-07 19:40:50 -0700

    Stores currencies in DB and fetches prices from DB

--- server/index.js
+++ server/index.js
@@ -9,6 +9,6 @@ const PUBLIC_DIRECTORY = path.join(__dirname, '../client/dist');
 app.use(express.static(PUBLIC_DIRECTORY));
 app.use(router);
 
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=> console.log(`Listening on Port ${PORT}`))
--- server/routers/pricesRouter.js
+++ server/routers/pricesRouter.js
@@ -5,8 +5,34 @@ const app = express();
 const router = express.Router();
 
 const axios = require('axios');
-const { formatPrice } = require('../helpers/helperFunctions.js')
+const { formatPrice } = require('../helpers/helperFunctions.js');
+const { save, retrieve } = require('../../database/index.js');
 
+const PAGE_SIZE = 100;
+
+let storePrices = function(data){
+  console.log('entering...')
+  let cryptoPrices = [];
+
+  //convert prices data to Mongo Schema
+  for (let i = 0; i < data.length; i++){
+
+    let crypto = {
+      rank: data[i].rank,
+      name: data[i].name,
+      symbol: data[i].symbol,
+      price: data[i].quotes.USD.price,
+      marketCap: data[i].quotes.USD.market_cap,
+      priceChange24hr: data[i].quotes.USD.percent_change_24h+"%",
+      priceChange1hr: data[i].quotes.USD.percent_change_1h+"%",
+      priceChange7d: data[i].quotes.USD.percent_change_7d+"%"
+    }
+    cryptoPrices.push(crypto); // add to array
+  }
+
+  save(cryptoPrices); //save prices in DB and send response object
+
+}
 
 //Get request for '/' all prices
 router.get('/', async (req, res)=>{
@@ -30,8 +56,10 @@ router.get('/', async (req, res)=>{
 
     cryptoData.sort((a,b)=>a.rank - b.rank); //sort prices
 
-
-    res.send(cryptoData);
+    storePrices(cryptoData);
+    let prices = await retrieve(PAGE_SIZE);
+    res.send(prices);
+    //res.send(cryptoData);
   }
 
   catch (err){
